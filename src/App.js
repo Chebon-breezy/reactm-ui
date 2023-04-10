d05e4e1948db68ab375542b726a0df593f491f26
Brian Chebon <brynnlamura@gmail.com>
2023-04-10 16:01:25 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup>
+          <Button
+            size="large"
+            startIcon={<SaveIcon />}
+            variant="contained" color="primary">
+            Save
+          </Button>
+
+          <Button
+            size="large"
+            startIcon={<DeleteIcon />}
+            variant="contained" color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
